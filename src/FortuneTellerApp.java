063ae7607190913ff61c601d322d3274fa8c6793
addiedavis <adeline.davis19@gmail.com>
2019-01-05 14:39:19 -0500

    ask age

--- src/FortuneTellerApp.java
+++ src/FortuneTellerApp.java
@@ -11,11 +11,12 @@ public class FortuneTellerApp {
 		System.out.println("Hello, what is your last name?");
 		String nameLast = input.next();
 		Thread.sleep(2000);
-		System.out.println("Welcome " + nameFirst.substring(0, 1).toUpperCase() + nameFirst.substring(1) + " " + nameLast.substring(0, 1).toUpperCase() + nameLast.substring(1) + ". Answer my questions three and I will tell what your future be.");
+		System.out.println("Welcome " + nameFirst.substring(0, 1).toUpperCase() + nameFirst.substring(1) + " " + nameLast.substring(0, 1).toUpperCase() + nameLast.substring(1) + ". Answer my questions four and I will tell what your in for.");
 		Thread.sleep(2000);
 		
-		
-		
+		System.out.println("Question one: How old are you?");
+		int age = input.nextInt();
+		System.out.println("You are " + age);
 		
 		
 		input.close();
