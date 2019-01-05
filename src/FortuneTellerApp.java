289eb425b0dfb91b4581f46f63c4a28ddfa11534
addiedavis <adeline.davis19@gmail.com>
2019-01-05 14:18:47 -0500

    Ask for name and welcome.

--- src/FortuneTellerApp.java
+++ src/FortuneTellerApp.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 
 public class FortuneTellerApp {
 
 	public static void main(String[] args) {
 		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Hello, who are you?");
+		String name = input.next();
+		
+		System.out.println("Welcome " + name + ". Answer my questions three and I will tell you what your future be.");
 		
 
 	}
